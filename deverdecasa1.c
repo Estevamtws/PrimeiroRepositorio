516a34c23c7e1a0cbb72001cc391c8877063900d
Pedro Estevam <pedro.estevam@sempreceub.com>
2025-03-20 18:36:00 -0300

    Delete deverdecasa1.c

--- deverdecasa1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main () {
-    float NotaA, NotaB, NotaC, MEDIA;
-    scanf("%f %f %f", &NotaA, &NotaB, &NotaC);
-    MEDIA = ((NotaA * 2) + (NotaB * 3) + (NotaC * 5)) / 10;
-    printf("MEDIA = %.1f\n", MEDIA);
-    return 0;
-}
